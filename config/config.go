cdfa69fae7dbcaa5242da4ddce370e17d5d5dc81
Boburjon Murodov <74082721+BoburjonMurodov@users.noreply.github.com>
2025-10-30 12:36:00 +0500

    feat: Setup environment variables
    - Loading sensitive info from .env files with Config

--- config/config.go
+++ config/config.go
@@ -9,7 +9,14 @@ type Config struct {
 
 func Load() *Config {
 	return &Config{
-		BotToken:    os.Getenv("BOT_TOKEN"),
-		GeminiToken: os.Getenv("GEMINI_TOKEN"),
+		BotToken:    getEnv("BOT_TOKEN", "YOUR_BOT_TOKEN"),
+		GeminiToken: getEnv("GEMINI_TOKEN", "YOUR_GEMINI_TOKEN"),
 	}
+}
+
+func getEnv(key, defaultVal string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return defaultVal
 }
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"AImBroke/config"
+	"fmt"
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Can't load .env file", err)
+		return
+	}
+	loadedConfig := config.Load()
+
+	fmt.Println("Loaded config: ", loadedConfig)
 }
